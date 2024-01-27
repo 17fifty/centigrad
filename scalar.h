aae2652ead62c2a30d43a1016b4eedc920423754
Joseph Daoud <josephdoaud@outlook.com>
2024-01-27 17:55:13 +0200

    progress

--- scalar.h
+++ scalar.h
@@ -10,83 +10,85 @@ namespace autodiff
 
     class Scalar
     {
-    private:
-        double value;
-        double grad;
-        Scalar* children[2] = {};
-        char op;
-        function<void()> _backwards;
+        private:
+            double value;
+            double grad;
+            Scalar* children[2] = {};
+            char op;
+            function<void()> _backwards;
 
-    public:
-        Scalar(double value, Scalar children[2] = {}, char op=' ')
-        {
-            this->value = value;
-            this->grad = 0;
-            *this->children = children;
-            this->op = op;
-        }
-
-        Scalar operator+(Scalar &other)
-        {
-            Scalar out(this->value + other.value, value, other.value, '+');
-            grad = 1;
-            out._backwards =[this, &other, out]()
+        public:
+            Scalar(double value, Scalar children[] = {}, char op=' ')
             {
-                this->grad += out.grad;
-                other.grad += out.grad;
-            };
-            return out;
-        }
-        Scalar operator*(Scalar &other)
-        {
-            Scalar out(value * other.value, value, other.value, '*');
-            grad = other.value;
-            other.grad = value;
-            return out;
-        }
-
-        Scalar operator*(double x)
-        {
-            Scalar out(value * x, value, x, '*');
-            return out;
-        }
-
-        Scalar operator-(Scalar &other)
-        {
-            return *this * -1;
-        }
-
-        Scalar OperatorBackward(Scalar &other, Scalar &out)
-        {
-            if (this->op == '+')
-            {
-                this->grad += out.grad;
-                other.grad += out.grad;
+                this->value = value;
+                this->grad = 0;
+                *this->children = children;
+                this->op = op;
             }
-            if (this->op == '*')
+
+            Scalar operator +(Scalar &other)
             {
-                this->grad += other.value * out.grad;
-                other.grad += this->value * out.grad;
+                Scalar set[]={*this, other};
+                Scalar out((value + other.value), set, '+');
+
+                out._backwards =[this, &other, out]()
+                {
+                    this->grad += out.grad;
+                    other.grad += out.grad;
+                };
+                return out;
             }
-            if (this->op == '^')
+
+            Scalar operator*(Scalar &other)
             {
-                this->grad += (other.value * pow(this->value, (other.value - 1)) * out.grad);
-                // TODO 1 - implement backwards function for power operations
+                Scalar out(value * other.value, value, other.value, '*');
+                grad = other.value;
+                other.grad = value;
+                return out;
             }
-        }
 
-        void Backward()
-        {
-            // TODO 2 - implement topo sort algo
+            Scalar operator*(double x)
+            {
+                Scalar out(value * x, value, x, '*');
+                return out;
+            }
 
-            this->grad = 1;
-            for (auto node : /*topoSortResult*/)
-                node.OperatorBackward(); // TODO 3 - find solution for calling other and out from here
-        }
+            Scalar operator-(Scalar &other)
+            {
+                return *this * -1;
+            }
 
-        void Print()
-        {
-            cout << "Deriv: " << grad << " Child 1 " << children[0] << " Child 2: " << children[1] << endl;
-        }
+            Scalar OperatorBackward(Scalar &other, Scalar &out)
+            {
+                if (this->op == '+')
+                {
+                    this->grad += out.grad;
+                    other.grad += out.grad;
+                }
+                if (this->op == '*')
+                {
+                    this->grad += other.value * out.grad;
+                    other.grad += this->value * out.grad;
+                }
+                if (this->op == '^')
+                {
+                    this->grad += (other.value * pow(this->value, (other.value - 1)) * out.grad);
+                    // TODO 1 - implement backwards function for power operations
+                }
+            }
+
+            void Backward()
+            {
+                // TODO 2 - implement topo sort algo
+
+                this->grad = 1;
+                for (auto node : /*topoSortResult*/)
+                    node.OperatorBackward(); // TODO 3 - find solution for calling other and out from here
+            }
+
+            void Print()
+            {
+                cout << "Deriv: " << grad << " Child 1 " << children[0] << " Child 2: " << children[1] << endl;
+            }
     };
 }
